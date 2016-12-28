2c9e54d83b9b3eaa53377c2b22eaf5be0b02d444
Umar Shehzad <umarshehzad@hotmail.com>
2016-12-28 13:40:36 +0500

    Other parameters from nmea string added

--- gps_new_code/gps_new_code.ino
+++ gps_new_code/gps_new_code.ino
@@ -6,6 +6,9 @@
     SoftwareSerial gpsSerial(10, 11); // create gps sensor connection
     TinyGPS gps; // create gps object
     long lat = 1000000, lon = 1000000;
+    unsigned long fix_age, time_, date, speed_, course_;
+    unsigned long chars;
+    unsigned short sentences, failed_checksum;
     
     void setup(){
       Serial.begin(9600); // connect serial
@@ -17,9 +20,26 @@
        if(gps.encode(gpsSerial.read())){ // encode gps data
         gps.get_position(&lat,&lon); // get latitude and longitude
         // display position
+        // time in hhmmsscc, date in ddmmyy
+      gps.get_datetime(&date, &time_, &fix_age);
+ 
+      // returns speed in 100ths of a knot
+      speed_ = gps.speed();
+ 
+      // course in 100ths of a degree
+      course_ = gps.course();
         Serial.print("Position: ");
         Serial.print("lat: ");Serial.print(lat);Serial.print(" ");// print latitude
-        Serial.print("lon: ");Serial.println(lon); // print longitude
+        Serial.print("lon: ");Serial.print(lon); Serial.print(" "); //print longitude
+        
+        Serial.print("fix_age: ");Serial.print(fix_age);Serial.print(" ");// print fix_age
+        Serial.print("time: ");Serial.print(time_); Serial.print(" "); // print time
+
+        Serial.print("date: ");Serial.print(date);Serial.print(" ");// print date
+        Serial.print("speed: ");Serial.print(speed_); Serial.print(" "); // print speed in knots
+
+        Serial.print("course: ");Serial.print(course_); Serial.println(" "); // print course
+        
        }
       }
     }
