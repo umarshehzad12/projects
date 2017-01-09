ed63aaf32fb3f5c3b93fee08fb7cdbe97b39838c
Umar Shehzad <umarshehzad@hotmail.com>
2017-01-09 06:44:01 +0000

    Added datetime and adjusted for better file formating

--- serial_test.py
+++ serial_test.py
@@ -1,16 +1,19 @@
 import serial
+import datetime
 
 ser = serial.Serial('/dev/ttyACM0',9600)
 s = []
+
 text_file=open("gyro_data1.txt", 'w')
 
 while True:
         #if ser.in_waiting():
                 #read_serial=ser.readline()
+        date_=str(datetime.datetime.now().time())
         s = ser.readline()
         print s
                 #print read_serial
-        text_file.write(s)
+        text_file.write(date_ + ", " + s)
 text_file.flush()
 text_file.close()
 ser.close()
